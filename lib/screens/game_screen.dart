6f1ef2b5b62ca9bb4d403969aa59ce3798de1698
eduzimmartins22 <Dudukof22@>
2025-03-25 22:50:06 -0300

    remapeado

---
+++ lib/screens/game_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import '../models/game_logic.dart';
+import '../widgets/score_board.dart';
+import '../widgets/game_options.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  final GameLogic _gameLogic = GameLogic();
+
+  void _play(String choice) {
+    setState(() {
+      _gameLogic.play(choice);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pedra, Papel e Tesoura'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: () => setState(() => _gameLogic.resetGame()),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            ScoreBoard(gameLogic: _gameLogic),
+            GameOptions(onPlay: _play),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/game_options.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class GameOptions extends StatelessWidget {
+  final Function(String) onPlay;
+
+  GameOptions({super.key, required this.onPlay}); 
+
+  final List<String> options = ['pedra', 'papel', 'tesoura'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: options.map((option) {
+        return GestureDetector(
+          onTap: () => onPlay(option),
+          child: Image.asset(
+            'assets/images/$option.png',
+            width: 100,
+            height: 100,
+            errorBuilder: (context, error, stackTrace) =>
+                const Icon(Icons.question_mark, size: 80),
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
+
